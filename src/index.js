1e43b6c2db14d9a51585631b360947e1c3375e8a
“rishikrv” <rishi.1496@gmail.com>
2025-04-20 13:25:17 +0530

    nejj

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const scrapeBlockedDates = require("./scraper");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import scrapeBlockedDates from "./scraper.js";
 
 const app = express();
 const port = 3000;
--- src/scraper.js
+++ src/scraper.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
 async function scrapeBlockedDates(url) {
   const browser = await puppeteer.launch({
@@ -39,4 +39,4 @@ async function scrapeBlockedDates(url) {
   }
 }
 
-module.exports = scrapeBlockedDates;
+export default scrapeBlockedDates;
